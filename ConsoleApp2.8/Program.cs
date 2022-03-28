65bee30118908887646d33cc01ff6acc715f969b
Alex Zverev <lordzver007@yandex.ru>
2022-03-28 23:17:01 +0300

    Добавьте файлы проекта.

---
+++ ConsoleApp2.8/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ConsoleApp2._8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int tryCount = 3;
+            string password = "123";
+            string userInput;
+
+            for(int i = 0; i < tryCount; i++)
+            {
+                Console.WriteLine("Введите пароль");
+                userInput = Console.ReadLine();
+
+                if (password == userInput)
+                {
+                    Console.WriteLine("Тайное сообщение");
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Осталось " + (tryCount - i - 1) + " попыток");
+                }
+            }            
+        }
+    }
+}
